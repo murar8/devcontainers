563cfa88c3a092eeca7bbd305a19142fc0e651f6
Lorenzo Murarotto <lnzmrr@gmail.com>
2020-11-05 16:39:33 +0100

    schedule daily update

--- scripts/build-push.sh
+++ scripts/build-push.sh
@@ -10,15 +10,18 @@ function usage() {
   echo
   echo "Arguments:"
   echo
-  echo "--build-context     Required    Working directory where docker will build the images."
   echo "--base-image        Required    Name of the root image on which the layers will be applied."
   echo "--image-name        Required    Name of the output image."
+  echo "--build-context     Required    Working directory where docker will build the images."
   echo "--layers            Required    Layers to be applied."
-  echo "--cache-repo        Required    Repository name where cache images will be stored."
+  echo "--push              false       Push the image to the registry after building it."
+  echo "--cache-repo                    Repository where cache images will be stored."
   echo "--tags              []          Space separated list of tags to add to the output image."
   echo "--labels            []          Space separated list of labels to add to the output image."
   echo
 }
+
+PUSH=false
 
 while (($#)); do
   case $1 in
@@ -41,6 +44,10 @@ while (($#)); do
       IMAGE_NAME="$2"
       shift
     fi
+    shift
+    ;;
+  --push)
+    PUSH=true
     shift
     ;;
   --cache-repo)
@@ -95,42 +102,46 @@ check $BUILD_CONTEXT
 check $BASE_IMAGE
 check $IMAGE_NAME
 check $LAYERS
-check $CACHE_REPO
 
 echo
 echo "Build context:      $BUILD_CONTEXT"
 echo "Base image:         $BASE_IMAGE"
 echo "Image name:         $IMAGE_NAME"
 echo "Layers:             ${LAYERS[@]}"
+echo "Push to registry:   $PUSH"
 echo "Cache repository:   $CACHE_REPO"
 echo "Tags:               ${TAGS[@]}"
 echo "Labels:             ${LABELS[@]}"
 echo
 
-tags=("$BASE_IMAGE")
+tags=("$BASE_IMAGE") # The tags of the parent image.
 
 for layer in ${LAYERS[@]}; do
+
   file="$layer/Dockerfile"
+
+  if ! [ -f $file ]; then
+    echo "Dockerfile not found for layer: $layer"
+    exit 1
+  fi
+
   last_tag="${tags[0]}"
 
   if [[ $layer == ${LAYERS[-1]} ]]; then
     tags=(${TAGS[@]/#/$IMAGE_NAME:})
   else
     checksum=$(echo -n $tag $layer | sha1sum | head -c 8)
-    tags=("$CACHE_REPO:$checksum")
+    tags=("${CACHE_REPO:-$IMAGE_NAME-build}:$checksum")
   fi
 
   echo "Adding layer '$layer' to '$last_tag'."
   echo
 
-  args=(
-    "$BUILD_CONTEXT"
-    "--file=$file"
-    "--build-arg=BASE_IMAGE=$last_tag"
-    "--cache-from=type=registry,ref=${tags[0]}"
-    "--cache-to=type=inline"
-    "--push"
-  )
+  args=("$BUILD_CONTEXT" "--file=$file" "--build-arg=BASE_IMAGE=$last_tag" "--push")
+
+  if [-n $CACHE_REPO]; then
+    args+=("--cache-from=type=registry,ref=${tags[0]}""--cache-to=type=inline")
+  fi
 
   DOCKER_CLI_EXPERIMENTAL=enabled docker buildx build \
     --progress plain \
