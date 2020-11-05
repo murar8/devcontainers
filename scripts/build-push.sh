d10adc7a208ec1e8c2db027eaa8765c602b009a8
Lorenzo Murarotto <lnzmrr@gmail.com>
2020-11-05 13:09:42 +0000

    use buildx

--- scripts/build-push.sh
+++ scripts/build-push.sh
@@ -4,28 +4,59 @@ set -e
 
 function usage() {
   echo
-  echo "Build a docker image composed of the provided layers and optionally push it to the supplied registry."
+  echo "Build a docker image composed of the provided layers and push it to the supplied registry."
   echo
-  echo "usage: $(basename $0) [options]... <build-context> <image-name> <base-image> <layers>..."
+  echo "usage: $(basename $0) [arguments]..."
   echo
-  echo "Positional arguments:"
+  echo "Arguments:"
   echo
-  echo "build-context               Working directory where docker will build the images."
-  echo "image-name                  Tag name of the output image."
-  echo "base-image                  Name of the root image on which the layers will be applied."
-  echo "layers                      Paths to the layers to be applied."
-  echo
-  echo "Named arguments:"
-  echo
-  echo "--tags                      Space separated list of tags to add to the output image."
-  echo "--labels                    Space separated list of labels to add to the output image."
-  echo "--push                      Push the images to the registry."
-  echo "--cache-name                Name of the cache image."
+  echo "--build-context     Required    Working directory where docker will build the images."
+  echo "--base-image        Required    Name of the root image on which the layers will be applied."
+  echo "--image-name        Required    Name of the output image."
+  echo "--layers            Required    Layers to be applied."
+  echo "--cache-repo        Required    Repository name where cache images will be stored."
+  echo "--tags              []          Space separated list of tags to add to the output image."
+  echo "--labels            []          Space separated list of labels to add to the output image."
   echo
 }
 
 while (($#)); do
   case $1 in
+  --build-context)
+    if [[ $2 != -* ]]; then
+      BUILD_CONTEXT="$2"
+      shift
+    fi
+    shift
+    ;;
+  --base-image)
+    if [[ $2 != -* ]]; then
+      BASE_IMAGE="$2"
+      shift
+    fi
+    shift
+    ;;
+  --image-name)
+    if [[ $2 != -* ]]; then
+      IMAGE_NAME="$2"
+      shift
+    fi
+    shift
+    ;;
+  --cache-repo)
+    if [[ $2 != -* ]]; then
+      CACHE_REPO="$2"
+      shift
+    fi
+    shift
+    ;;
+  --layers)
+    while [[ "$#" -ge 2 && "$2" != -* ]]; do
+      LAYERS+=("$2")
+      shift
+    done
+    shift
+    ;;
   --tags)
     while [[ "$#" -ge 2 && "$2" != -* ]]; do
       TAGS+=("$2")
@@ -40,98 +71,74 @@ while (($#)); do
     done
     shift
     ;;
-  --push)
-    PUSH=true
-    shift
-    ;;
-  --cache-name)
-    if [[ $2 != -* ]]; then
-      CACHE_NAME="$2"
-      shift
-    fi
-    shift
-    ;;
   --)
     shift
     ;;
-  -*)
+  --* | -*)
     echo
     echo "Unknown option: $1"
     usage
     exit 1
-    shift
-    ;;
-  *)
-    POSITIONAL_ARGS+=("$1")
-    shift
     ;;
   esac
-
 done
 
-if [ "${#POSITIONAL_ARGS[@]}" -lt 4 ]; then
-  echo "Not enough arguments provided."
-  exit 1
-fi
+check() {
+  if [ -z $1 ]; then
+    echo "Missing required argument."
+    usage
+    exit 1
+  fi
+}
 
-BUILD_CONTEXT="${POSITIONAL_ARGS[0]}"
-BASE_IMAGE="${POSITIONAL_ARGS[1]}"
-IMAGE_NAME="${POSITIONAL_ARGS[2]}"
-LAYERS="${POSITIONAL_ARGS[@]:3}"
+check $BUILD_CONTEXT
+check $BASE_IMAGE
+check $IMAGE_NAME
+check $LAYERS
+check $CACHE_REPO
 
 echo
 echo "Build context:      $BUILD_CONTEXT"
-echo "Image name:         $IMAGE_NAME"
 echo "Base image:         $BASE_IMAGE"
+echo "Image name:         $IMAGE_NAME"
 echo "Layers:             ${LAYERS[@]}"
+echo "Cache repository:   $CACHE_REPO"
 echo "Tags:               ${TAGS[@]}"
 echo "Labels:             ${LABELS[@]}"
-echo "Push to registry:   ${PUSH}"
-echo "Cache name:         ${CACHE_NAME}"
 echo
 
-tag="$BASE_IMAGE"
+tags=("$BASE_IMAGE")
 
 for layer in ${LAYERS[@]}; do
-  file="$BUILD_CONTEXT/$layer/Dockerfile"
-  checksum=$(echo -n $tag $layer | sha1sum | head -c 8)
-  last_tag="$tag"
-  tag="${CACHE_NAME:-$IMAGE_NAME-build}:$checksum"
+  file="$layer/Dockerfile"
+  last_tag="${tags[0]}"
+
+  if [[ $layer == ${LAYERS[-1]} ]]; then
+    tags=(${TAGS[@]/#/$IMAGE_NAME:})
+  else
+    checksum=$(echo -n $tag $layer | sha1sum | head -c 8)
+    tags=("$CACHE_REPO:$checksum")
+  fi
 
   echo "Adding layer '$layer' to '$last_tag'."
+  echo
 
   args=(
     "$BUILD_CONTEXT"
     "--file=$file"
     "--build-arg=BASE_IMAGE=$last_tag"
-    "--tag=$tag"
-    "--cache-from=$tag"
+    "--cache-from=type=registry,ref=${tags[0]}"
+    "--cache-to=type=inline"
+    "--push"
   )
 
-  docker build --quiet "${args[@]}" "${LABELS[@]/#/--label=}" 1>/dev/null
-
-  echo "Successfully built '$tag'."
-
-  if ! [ -z $CACHE_NAME ]; then
-    echo "Pushing '$tag' to the registry."
-    docker push $1 "$tag" 1>/dev/null
-  fi
+  DOCKER_CLI_EXPERIMENTAL=enabled docker buildx build \
+    --progress plain \
+    "${args[@]}" \
+    "${LABELS[@]/#/--label=}" \
+    "${tags[@]/#/--tag=}"
 
   echo
+  echo "Successfully built '${tags[0]}'."
+  echo
 done
-
-tags=(${TAGS[@]/#/$IMAGE_NAME:})
-
-for ptag in ${tags[@]}; do
-  echo "Adding tag '$ptag' to '$tag'."
-  docker tag "$tag" "$ptag"
-done
-
-echo
-
-if [ $PUSH = true ]; then
-  for ptag in ${tags[@]}; do
-    echo "Pushing '$ptag' to registry."
-    docker push "$ptag" 1>/dev/null
-  done
-fi
